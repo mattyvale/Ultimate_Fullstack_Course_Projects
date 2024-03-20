dcb626c431311c6827a890364695c709e93b28ee
Matthew Vale <mattyvale-94@hotmail.co.uk>
2024-03-20 20:46:57 +0000

    Updated PokemonApp.py to handle request errors

--- Python 201/PokemonApp.py
+++ Python 201/PokemonApp.py
@@ -1,16 +1,24 @@
 import requests
 import json
 
-chosen_pokemon = input("Enter the name of your chosen Pokemon! ")
-chosen_pokemon = chosen_pokemon.lower()
-url = f'https://pokeapi.co/api/v2/pokemon/{chosen_pokemon}'
-req = requests.get(url)
-pokemon_data = req.json()
+while True:
+    chosen_pokemon = input("Enter the name of your chosen Pokemon! ")
+    chosen_pokemon = chosen_pokemon.lower()
 
-print(f"You chose {pokemon_data['name']}!")
-print("Abilities:")
-for ability in pokemon_data['abilities']:
-    print(ability['ability']['name'])
-print("Held items:")
-for item in pokemon_data['held_items']:
-    print(item['item']['name'])
+    url = f'https://pokeapi.co/api/v2/pokemon/{chosen_pokemon}'
+
+    req = requests.get(url)
+    if req.status_code == 200:
+        pokemon_data = req.json()
+
+        print(f"You chose {pokemon_data['name']}!")
+
+        print("Abilities:")
+        for ability in pokemon_data['abilities']:
+            print(ability['ability']['name'])
+
+        print("Held items:")
+        for item in pokemon_data['held_items']:
+            print(item['item']['name'])
+    else:
+        print("Chosen pokemon doesn't exist!")
